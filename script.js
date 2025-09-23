16ef835752adc9feceffcdb04529d423b3a31678
Aryan-Scripter <aryansinha.123.as@gmail.com>
2025-09-23 20:52:35 +0530

    - Implemented JavaScript for the calculator screen - Updated CSS styling for the screen - Adjusted related HTML structure

---
+++ script.js
@@ -0,0 +1,35 @@
+const keys = document.querySelectorAll(".key");
+const screen = document.querySelector(".screen");
+// const operator = document.querySelectorAll(".operator");
+let screenNum = [];
+const operator = ["+", "-", "*", "/"];
+
+
+keys.forEach(e => {
+    e.onclick = () => {
+        if(e.dataset.key != "ce" && e.dataset.key != "=") {
+            let lastElement = screenNum.length;
+            let value = e.dataset.key;
+
+             if(screenNum.length === 0 && operator.includes(value)) {
+                return;
+            }
+            
+            if(operator.includes(value)) {
+                if(operator.includes(screenNum[lastElement - 1])) { //checks if
+                    screenNum[lastElement - 1] = value; //overtides the operator
+                }
+                else {
+                    screenNum.push(value);
+                }
+            }
+            else {
+                screenNum.push(value);
+            }
+            // console.log(screenNum.length);
+            console.log(operator.includes(value));
+            screen.textContent = screenNum.join("");
+        }
+    }
+});
+
